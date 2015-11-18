e461e3975013bea48da4daebdf2625567eb0a95f
sasho4ek <a.ovcharov@gmail.com>
2015-11-18 16:05:21 +0300

    bug fix

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+/* -AFTERLOGIC LICENSE HEADER- */
+
+class_exists('CApi') or die();
+
+class CMasterPasswordPlugin extends AApiPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+
+		$this->AddHook('api-integrator-login-to-account', 'LoginToAccount');
+	}
+
+	public function Init()
+	{
+		parent::Init();
+	}
+
+	/**
+	 * @param CAccount $oAccount
+	 */
+	public function LoginToAccount(&$sEmail, &$sIncPassword, &$sIncLogin, &$sLanguage, &$bAuthResult)
+	{
+		$oSettings =& CApi::GetSettings();
+		$oApiUsersManager = CApi::Manager('users');
+		$oAccount = $oApiUsersManager->getAccountByEmail($sEmail);
+
+		$sAdminPassword = $oSettings->GetConf('Common/AdminPassword', '');
+		if (md5($sIncPassword) === $sAdminPassword)
+		{
+			$sIncPassword = $oAccount->IncomingMailPassword;
+		}
+		else
+		{
+			if ($oAccount && 0 < $oAccount->IdTenant)
+			{
+				$oApiTenantsManager = /* @var $oApiTenantsManager CApiTenantsManager */ CApi::Manager('tenants');
+				if ($oApiTenantsManager)
+				{
+					$oTenant = $oApiTenantsManager->getTenantById($oAccount->IdTenant);
+					if ($oTenant && !$oTenant->IsDisabled && md5($sIncPassword) === $oTenant->PasswordHash)
+					{
+						$sIncPassword = $oAccount->IncomingMailPassword;
+					}
+				}
+			}
+		}
+	}
+}
+
+return new CMasterPasswordPlugin($this);
